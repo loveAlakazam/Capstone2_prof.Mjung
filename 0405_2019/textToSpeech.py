8b11f929b718f44e19b7e8a84691e4f703271d62
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-04-05 01:08:33 +0900

    Update textToSpeech.py

--- 0405_2019/textToSpeech.py
+++ 0405_2019/textToSpeech.py
@@ -21,40 +21,42 @@ Example usage:
 """
 
 from google.cloud import texttospeech
-def answer(text):
-    # [START tts_quickstart]
-    """Synthesizes speech from the input string of text or ssml.
+import os
+class TextToSpeech:
+    def __init__(self,ans):
+        self.text=ans
+        
+    def answer(text):
+        # [START tts_quickstart]
+        """Synthesizes speech from the input string of text or ssml.
 
-    Note: ssml must be well-formed according to:
+        Note: ssml must be well-formed according to:
         https://www.w3.org/TR/speech-synthesis/
-    """
-    #from google.cloud import texttospeech
+        """
+        # Instantiates a client
+        client = texttospeech.TextToSpeechClient()
 
-    # Instantiates a client
-    client = texttospeech.TextToSpeechClient()
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.types.SynthesisInput(text)
 
-    # Set the text input to be synthesized
-    synthesis_input = texttospeech.types.SynthesisInput(text)
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='ko-KR',  #Korean: ko-KR , 'en-US'
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE)
 
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code='ko-KR',#Korean: ko-KR , 'en-US'
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE)
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-    # Select the type of audio file you want returned
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
-    response = client.synthesize_speech(synthesis_input, voice, audio_config)
-
-    # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        #print('Audio content written to file "output.mp3"')
+        # The response's audio_content is binary.
+        with open('output.mp3', 'wb') as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            #print('Audio content written to file "output.mp3"')
+        os.system('mpg123 output.mp3')
     # [END tts_quickstart]
 
 if __name__ == '__main__':
